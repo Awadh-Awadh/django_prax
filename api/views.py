435e9a01b302bb91df8f6cfd0ccaea30b9425cb2
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-13 18:05:30 +0300

    add urlConf and view function

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def api_overview(request):
+    api_urls = {
+      'list':'/product-list/',
+      'detail-view':'/product-detail/<int:id>/',
+      'create':'/product-create/',
+      'update':'product-update/<int:id>/',
+      'delete': 'product-delete/<int:id>/'
+    }
